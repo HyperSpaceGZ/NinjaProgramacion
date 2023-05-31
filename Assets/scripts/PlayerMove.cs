e73d5217e272cf06b5fdd03606384a728a547872
Alejandro Nicolas Biga <alejandro.z.biga@gmail.com>
2023-05-31 17:19:20 -0300

    move dont work
    i try to set animator but dont work

--- Assets/scripts/PlayerMove.cs
+++ Assets/scripts/PlayerMove.cs
@@ -7,9 +7,15 @@ public class PlayerMove : MonoBehaviour
     public float moveSpeed = 5f;
     private Transform playerTransform;
 
+    //animation
+    private Animator animator;
+    private bool isMoving;
+
     private void Start()
     {
         playerTransform = transform;
+        animator = GetComponent<Animator>();
+        isMoving = false;
     }
 
     private void Update()
@@ -20,11 +26,26 @@ public class PlayerMove : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0f);
         playerTransform.position += movement * moveSpeed * Time.deltaTime;
 
+        
+
         // Aiming
         Vector3 mousePosition = Input.mousePosition;
         Vector3 screenPoint = Camera.main.WorldToScreenPoint(playerTransform.localPosition);
         Vector2 aimDirection = (mousePosition - screenPoint).normalized;
         float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
         playerTransform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        // Check if there is any input for movement
+        if (moveHorizontal != 0f || moveVertical != 0f)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+
+        // Set the "isMoving" parameter in the Animator controller
+        animator.SetBool("isMoving", isMoving);
     }
 }
