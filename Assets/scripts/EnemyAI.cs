273faad9e4f6ed43dd662022d0bf7511236ad9bf
Thomás Alonso <96887716+HyperSpaceGZ@users.noreply.github.com>
2023-06-20 17:48:04 -0300

    script ai

--- Assets/scripts/EnemyAI.cs
+++ Assets/scripts/EnemyAI.cs
@@ -9,22 +9,35 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] private NavMeshAgent EnemyNavMesh;
     [SerializeField] private bool hastriggered;
 
+    [SerializeField]  private Animator animator;
+
     [SerializeField] private int enemyHP;
- 
+
+
+    void FixedUpdate()
+    {
+
+    }
 
     void Start()
     {
+        //Gets the enemy navmesh and animator components 
         EnemyNavMesh = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+
+        //Fixes a bug where the sprite is in the wrong rotation
         EnemyNavMesh.updateRotation = false;
         EnemyNavMesh.updateUpAxis = false;
 
         hastriggered = false;
 
+        //Gets the players transform for the navigation AI
         PlayerTransform = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        //Collisions when is hits or hits the player
         if (collision.gameObject.tag == "bullet")
         {
 
@@ -33,6 +46,7 @@ public class EnemyAI : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //Sets the destination to the player ONLY ONCE when it gets inside the trigger.
         if (hastriggered == false && collision.gameObject.tag == "Player")
         {
             hastriggered = true;
