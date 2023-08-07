cf5b910ed2ae9384a3d32d1c831a42f97c9bf8c5
Alejandro Nicolas Biga <alejandro.z.biga@gmail.com>
2023-08-07 10:39:27 -0300

    bullet FX

--- Assets/scripts/BulletScript.cs
+++ Assets/scripts/BulletScript.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class BulletScript : MonoBehaviour
 {
 
+    //bullet effect
+    public GameObject hitEffect;
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.GetComponent<Ienemydmg>()!= null)
         {
             collision.gameObject.GetComponent<Ienemydmg>().EnemyDamage();
         }
-      
+
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 0.12f);
+        Destroy(gameObject);
+
     }
 }
